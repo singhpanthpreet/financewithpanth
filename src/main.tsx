ad040f3d2cc26097a978df624f22d171c1b8a9da
singhpanthpreet <financewithpanth@gmail.com>
2026-03-11 17:12:13 -0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import FinanceWithPanthAboutPage from "./App";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
+import AIpage from "./AIpage";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <FinanceWithPanthAboutPage />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/ai" element={<AIpage />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
