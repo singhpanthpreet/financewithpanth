bc246907336af9489a782f7fa93687e80d7a7478
singhpanthpreet <financewithpanth@gmail.com>
2026-03-19 14:40:41 -0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
-import AIpage from "./AIpage";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/ai" element={<AIpage />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
